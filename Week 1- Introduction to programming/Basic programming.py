9a5a15567760ca4160e971e7b236ea9559a586d4
peds12342 <pedrobengalinha123@gmail.com>
2023-10-24 20:54:16 +0100

    Did all the practical tasks for week 1, very basic calculations and learning functions such as print.

---
+++ Week 1- Introduction to programming/Basic programming.py
@@ -0,0 +1,31 @@
+print("The program has executed")
+
+print("Hello World")
+
+print("Hello, fellow programmers.")
+
+print("This is th work for my week 1 practical :)")
+
+help("keywords")
+
+help(input)
+
+print(45+20)
+print(0 + 20 - 15)
+print(10 * 5)
+print(100 / 33)
+print(100 // 33)
+print(10 ** 2)
+print(10 % 3)
+
+print(10 + 5 * 2)
+print(10 - 5 * 10 + 5)
+print(5 * 10 ** 2)
+
+print((10 + 5) * 2)
+print(10 - 5 * (10 + 5))
+print((5 * 10) ** 2)
+
+print(12 + (5 * 2 + 3))
+print(12 + (5 * (2 + 3)))
+
