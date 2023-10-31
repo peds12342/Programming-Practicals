f468b42fc97712107f8bb34f9b72886ced957895
peds12342 <pedrobengalinha123@gmail.com>
2023-10-31 14:52:41 +0000

    Have now completed all the tasks of week 6.

--- Week 6- Lists and tuples/Lists and Tuples tasks.py
+++ Week 6- Lists and tuples/Lists and Tuples tasks.py
@@ -58,3 +58,45 @@ print(names)
 
 names.sort(key=lambda x: x.upper())
 print(names)
+
+
+squares.reverse()
+print(squares)
+
+
+colours = ["red", "green", "yellow", "blue", "red"]
+index_of_blue = colours.index("blue")
+print(index_of_blue)
+
+
+colours_copy = colours.copy()
+colours[0] = "orange"
+colours.append("purple")
+print("Original List:", colours_copy)
+print("New List:", colours)
+
+
+cubes = [x ** 3 for x in range(2, 21)]
+print(cubes)
+
+
+all_users = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "George", "Hannah"]
+some_users = [u for u in all_users if len(u) < 8]
+print(f"These users met the requirements to be on the list: {some_users}")
+#This code makes it so that any usernames in all_users that have less than 8 characters will enter the some_users list.
+
+
+address = (8, "Ham", "12345")
+house_number, street, postcode = address
+
+
+the_one = ("Neo",)
+print(the_one)
+
+print("House Number:", house_number)
+print("Street:", street)
+print("Postcode:", postcode)
+
+
+print(*address)
+print(address)
