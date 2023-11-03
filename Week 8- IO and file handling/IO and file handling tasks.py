7c91b4991e4950136cd74a1ab97181e747080a04
peds12342 <pedrobengalinha123@gmail.com>
2023-11-03 17:28:42 +0000

    Have now completed all the tasks for week 8.

--- Week 8- IO and file handling/IO and file handling tasks.py
+++ Week 8- IO and file handling/IO and file handling tasks.py
@@ -9,9 +9,39 @@ age = int(input("Please enter your age: "))
 print(f"{name:15} - {age:10}")
 print(f"{name:10} - {age:20}")
 print(f"{name:20} - {age:30}")
-print(f"{name:*>20} - ${age:.2f:>20}")
+print(f"{name:*>20} - ${age:.2f}")
 
 
 r = 52
 area = 3.14159 * r**2
 print(f"The area of a circle with a radius of {r} is {area:.2f} square units.".format(r=r, area=area))
+
+
+f_string = "{name:@^15} - {age:#^10}".format(name=name, age=age)
+print(f_string)
+
+
+f = open("info.txt")
+file_contents = f.read()
+f.close()
+
+
+with open("info.txt", "r") as file:
+    line = file.readline()
+    while line:
+        print(line, end="")
+        line = file.readline()
+
+
+with open("info.txt", "r") as file:
+    for line in file:
+        print(line, end="")
+
+
+with open("info.txt", "a") as file:
+    file.write("This is an extra line\n")
+
+
+with open("info.txt", "r") as file:
+    for line in file:
+        print(line, end="")
